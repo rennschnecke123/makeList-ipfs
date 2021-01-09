435e87e7e8bf28e3c06268885e090942d56bb091
Reiner Rusch <rusch.reiner@web.de>
2021-01-09 02:53:54 +0100

    Update makeList-ipfs.sh
    calculating redundancy:
    -q : timeout 10s
    -v : no timeout
    none: 0s timeout, so no calculation

--- makeList-ipfs.sh
+++ makeList-ipfs.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+par=$1
+
+if [ "$par" == "-q" ];
+then
+	timeout=10
+elif [ "$par" == '-v' ]
+then
+	timeout=600
+else
+	timeout=0
+fi
+
 date=$(date +%s)
-file=$1
+file=$2
 if [ "$file" == "" ];
 then
 listCommand="ipfs pin ls -t recursive -q"
@@ -11,7 +23,7 @@ fi
 echo "<html><body><h3>IPFS Linklist</h3><ul><small>" > /tmp/$date.html
 echo "IPFS Linklist" > /tmp/ipfs.txt
 touch ~/.cache/ipfs.cache
-for item in $($listCommand|sort|uniq); do newName=0; name=$(cat ~/.cache/ipfs.cache|grep $item|head -n1| cut -d';' -f2 2>/dev/null|sed s/\"//g); if [ "$name" == "" ]; then name=$(ipfs ls $item 2>/dev/null| cut -d' ' -f3-|sed s/\"//g); newName=1; fi; if [ "$name" == "" ]; then name="[unknown object]"; fi; echo "<li><b><a href=\"/ipfs/$item\" target=\"_blank\" style=\"text-decoration: none;\">${name:0:80}</a></b>" >> /tmp/$date.html; echo "$item - $name" >> /tmp/ipfs.txt; if [ "$newName" == "1" ]; then echo "\"$item\";\"$name\";\"https://ipfs.io/ipfs/$item\";\""$(date +%s)"\"" >> ~/.cache/ipfs.cache; fi; done
+for item in $($listCommand|sort|uniq); do newName=0; name=$(cat ~/.cache/ipfs.cache|grep $item|head -n1| cut -d';' -f2 2>/dev/null|sed s/\"//g); if [ "$name" == "" ]; then name=$(ipfs ls $item 2>/dev/null| cut -d' ' -f3-|sed s/\"//g); newName=1; fi; if [ "$name" == "" ]; then name="[unknown object]"; fi; echo "<li><b><a href=\"/ipfs/$item\" target=\"_blank\" style=\"text-decoration: none;\">${name:0:80}</a></b>" >> /tmp/$date.html; echo "$item - $name" >> /tmp/ipfs.txt; if [ "$newName" == "1" ]; then echo "\"$item\";\"$name\";\"https://ipfs.io/ipfs/$item\";\""$(date +%s)"\";\""$(timeout $timeout ipfs dht findprovs $item 2>/dev/null | wc -l)"\"" >> ~/.cache/ipfs.cache 2>/dev/null; fi; done
 echo "</ul></small></body></html>" >> /tmp/$date.html
 newItem=$(ipfs add /tmp/$date.html | tail -n1 | cut -d ' ' -f 2)
 echo "local: http://localhost:8080/ipfs/$newItem"
@@ -20,6 +32,4 @@ ipfs pin rm $newItem >/dev/null
 rm /tmp/$date.html >/dev/null
 
 
-firefox http://localhost:8080/ipfs/$newItem &
-
-
+firefox http://localhost:8080/ipfs/$newItem 2>/dev/null &
